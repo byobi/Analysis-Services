9d611163f4206902c4321ca74f07a19f95688c2a
Bill Anton <anton@opifexsolutions.com>
2017-11-28 08:12:50 -0500

    added posh script to test AzureAS service credential to be used w/ AzureFunction

---
+++ AsPartitionProcessing/MiscScripts/PoSh - Test AzureAS Service Credential.ps1
@@ -0,0 +1,18 @@
+﻿Install-Module -Name Azure.AnalysisServices
+Install-Module -Name SqlServer
+
+Update-Module -Name Azure.AnalysisServices
+Update-Module -Name SqlServer
+
+
+$AppId = "9964d45f-ab47-4b2e-804d-16ccc0743e62"
+$PlainPWord = "vFnC2OQ5bouGK3yiteKkBv1DXKwG2ZTZ+9Jfso8wi34="
+$TenantId = "54fd453c-3cc6-41e8-8200-fa568f04164a"
+
+$PWord = ConvertTo-SecureString -String $PlainPWord -AsPlainText -Force
+
+$Credential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $AppId, $PWord
+
+Login-AzureAsAccount -Credential $Credential -ServicePrincipal -TenantId $TenantId -RolloutEnvironment "southcentralus.asazure.windows.net"
+
+Invoke-ProcessTable -Server "asazure://southcentralus.asazure.windows.net/opifexdemotabular" -TableName "Date" -Database "ASPP_AdventureWorksDW1200" -RefreshType "Full"
